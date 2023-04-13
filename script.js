01306c778b11cf6c56775a624ce8483af63169ed
aitestbed <128209439+aitestbed@users.noreply.github.com>
2023-04-13 12:04:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -96,14 +96,19 @@ async function search() {
     }
   }
 
-  const editorialResults = await getResults("format:oba.nl%20" + searchTerm, "", "&pagesize=1");
-  if (editorialResults.length > 0) {
-    const editorialResult = editorialResults[0];
-    const summary = editorialResult.summaries[0];
-    const detailLink = editorialResult.detailLink;
-    chatMessages.innerHTML += `<div class="chat-message bot"><span>${summary} <a href="${detailLink}" target="_blank">Meer</a></span></div>`;
-  } else {
-    chatMessages.innerHTML += '<div class="chat-message bot"><span>Geen redactionele inhoud gevonden. Kan ik nog iets voor je zoeken?</span></div>';
+  chatMessages.innerHTML += '<div class="chat-message bot"><span>Hier zijn de resultaten van je zoekvraag voor "' + searchTerm + '". Kan ik nog iets voor je zoeken?</span></div>';
+
+  const keywords = ["openingstijden", "open", "geopend"];
+  if (keywords.some((word) => searchTerm.toLowerCase().includes(word.toLowerCase()))) {
+    const editorialResults = await getResults("format:oba.nl%20" + searchTerm, "", "&pagesize=1");
+    if (editorialResults.length > 0) {
+      const editorialResult = editorialResults[0];
+      const summary = editorialResult.summaries[0];
+      const detailLink = editorialResult.detailLink;
+      chatMessages.innerHTML += `<div class="chat-message bot"><span>${summary} <a href="${detailLink}" target="_blank">Meer</a></span></div>`;
+    } else {
+      chatMessages.innerHTML += '<div class="chat-message bot"><span>Geen redactionele inhoud gevonden. Kan ik nog iets voor je zoeken?</span></div>';
+    }
   }
 
   chatBody.scrollTop = chatBody.scrollHeight;
