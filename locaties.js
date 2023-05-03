e73a0590bb5d611ae7be2d95445e17c012d5a683
aitestbed <128209439+aitestbed@users.noreply.github.com>
2023-05-03 06:52:26 +0200

    Update locaties.js

--- locaties.js
+++ locaties.js
@@ -13,23 +13,57 @@ document.addEventListener('DOMContentLoaded', () => {
 
   postcodeInput.addEventListener('input', updateMap);
 
-  function updateMap() {
+  function generateUrl() {
+    const locationParams = {
+      Noord: 'an',
+      Zuid: 'az',
+      Zuidoost: 'azo',
+      'Nieuw-West': 'anw',
+      West: 'aw',
+      Oost: 'ao',
+    };
+
+    const categoryParams = {
+      Taal: 't',
+      Geld: 'g',
+      Digitaal: 'd',
+    };
+
     const selectedLocations = Array.from(locationCheckboxes)
       .filter((checkbox) => checkbox.checked)
-      .map((checkbox) => checkbox.value);
+      .map((checkbox) => locationParams[checkbox.value]);
 
     const selectedCategories = Array.from(categoryCheckboxes)
       .filter((checkbox) => checkbox.checked)
-      .map((checkbox) => checkbox.value);
+      .map((checkbox) => categoryParams[checkbox.value]);
 
     const postcode = postcodeInput.value;
 
+    const allLocations = Object.values(locationParams);
+
+    const activateParams = selectedLocations.length === 0 ? allLocations : selectedLocations;
     const queryParams = new URLSearchParams();
-    if (selectedLocations.length > 0) queryParams.append('locations', selectedLocations.join(','));
-    if (selectedCategories.length > 0) queryParams.append('categories', selectedCategories.join(','));
+
     if (postcode) queryParams.append('postcode', postcode);
 
-    const mapUrl = 'https://localfocuswidgets.net/6451218aa5b9e?activate=asa';
-    document.getElementById('map').src = `${mapUrl}&${queryParams.toString()}`;
+    const mapUrl = 'https://localfocuswidgets.net/6451e73d1d2b8?hide=dropdowns';
+    const combinations = [];
+
+    for (const location of activateParams) {
+      const categories = selectedCategories.length === 0 ? [''] : selectedCategories;
+      for (const category of categories) {
+        combinations.push(`${location}${category}`);
+      }
+    }
+
+    for (const combination of combinations) {
+      queryParams.append('activate|sa', combination);
+    }
+
+    return `${mapUrl}?${queryParams.toString()}`;
+  }
+
+  function updateMap() {
+    document.getElementById('map').src = generateUrl();
   }
 });
